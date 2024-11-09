e9045b46bfa68687428930ddfe85a43eeaa8ba28
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2024-11-09 00:35:45 +0000

    day6 updated

--- .vscode/day06/pt1.rb
+++ .vscode/day06/pt1.rb
@@ -3,22 +3,21 @@
 # define how the instructions are executed
 # figure out how to apply them to a square for which you have two coordinates and need to extrapolate the other two
 
-string.split
-instructions, points = line.split("") #difficulty here is what to split on. Unresolved!
-start_point, end_point = points.split(" through ")
-approved_start_date, approved_end_date = approved_between.split(" - ")
-
+def extract_data (instruction, start_point, end_point)
+    instruction, points = line.split("") #difficulty here is what to split on. Unresolved!
+    start_point, end_point = points.split(" through ")
+end
 
 def execute_instruction
     if instruction = 'turn on'
-        status = on
+        1
     elsif instruction = 'turn off'
-        status = off
+        0
     else #i.e. toggle
-        if status = off
-            status = on
+        if 0
+            1
         else
-            status = off
+            0
         end
     end
 end
