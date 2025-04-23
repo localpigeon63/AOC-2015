6fed3a63beee6cec83561f49eb51ed9a925205b5
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-04-23 21:51:02 +0000

    Day 12 Pt 2 working for examples, not for puzzle input

--- day12/pt2.rb
+++ day12/pt2.rb
@@ -1,5 +1,10 @@
 input = File.read("./day12/input.txt").slice(1..-2)
 
+input1 = '[1,2,3]'
+input2 = '[1,{"c":"red","b":2},3]'
+input3 = '{"d":"red","e":[1,2,3,4],"f":5}'
+input4 = '[1,"red",5]'
+
 totals = []
 
 def extract_numbers(x)
@@ -15,28 +20,33 @@ string = ''
 strings = []
 
 input.each_char do |c|
-    if c.match('{')
+    if c == '{'
         open_parentheses = open_parentheses + 1
-    elsif c.match('}')
+    elsif c == '}'
         close_parentheses = close_parentheses + 1
     end
+    string << c
     if open_parentheses == close_parentheses && open_parentheses > 0
-        string << c
         strings << string
         string = ''
         open_parentheses = 0
-        close_parentheses = 0
-    else
-        string << c
+        close_parentheses = 0 
     end
 end
 
+strings << string.strip unless string.strip.empty?
+
 pp strings.count
+pp strings
 
 strings.each do |line|
     if 
         !line.include?('red') ||
-        line.count('red') == line.scan(/\[[^\]]*\bred\b[^\]]*\]/).length
+        line.count('red') == line.scan(/\[[^\[\]{}]*"red"[^\[\]{}]*\]/).length
+        totals << extract_numbers(line).sum
+    else
+        line.gsub!(/\{[^{}]*"red"[^{}]*\}/, '')
+        puts line.inspect
         totals << extract_numbers(line).sum
     end
 end
