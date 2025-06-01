eb8425f376b59f62633f2f6612daff803d2acbc6
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-06-01 14:52:54 +0000

    day20 pt1 working but very slow

--- day20/pt1.rb
+++ day20/pt1.rb
@@ -1,14 +1,19 @@
-houses = []
+houses = Array.new(1000000, 0)
 
-elf_number = 0
+elf_numbers = 1..1000000
 
-while elf_number < 10
-houses.each_with_index do |house, i|
-    if i % elf_number == 0
-        house = house + (elf_number * 10)
+results = []
+
+elf_numbers.each do |elf_number|
+    houses.each_with_index do |house, i|
+        if i > 0 &&
+            (i % elf_number) == 0
+            houses[i] = house + (elf_number * 10)
+        end
+        if house >= 33100000
+            results << i
+        end
     end
 end
-elf_number = elf_number + 1
-end
 
-pp houses
+puts results.min
