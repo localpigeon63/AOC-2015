cc4fcdb390c52206493fb4f77a93a8bbde8b9d58
localpigeon63 <152446902+localpigeon63@users.noreply.github.com>
2025-06-11 20:05:30 +0000

    day 25 complete

--- day25/pt1.rb
+++ day25/pt1.rb
@@ -1,47 +1,27 @@
 row = 1
 column = 1
 value = 20151125
-max_row = 1
 target_row = 2947
 target_column = 3029
+multiplier = 252533
+modulo = 33554393
 
-until row ==target_row &&
-    column == target_column do
-        row = row - 1
-        if row < 1
-            max_row = max_row + 1
-            row = max_row
-        end
-        column = column + 1
-        if column > max_row -1
-            column = 1
-        end
-        value = (value * 252533) % 33554393
-end 
+# number of entries in each diagonal is equal to the sum of each pair of indices, -1
+current_diagonal = (target_row + target_column) - 1
+
+#formula for calculating triangular numbers, calculating number of indices in all complete diagonals
+previous_indices = ((current_diagonal - 1) * current_diagonal) / 2
+
+# works out how far into current (incomplete) diagonal the target position lies
+offset = current_diagonal - target_row
+
+# adds up how far into the total number list the target position lies
+target_position = previous_indices + offset + 1
+
+(target_position - 1).times do 
+    value = (value * multiplier) % modulo
+end
 
 puts value
 
-
-
-#1.	Initialise infinite grid (i.e. a hash)
-# don't actually need a hash - just needs variables, with row = 1, column = 1, value = starting value
-
-#3.	Start loop (while indices are less than target index do – remember target index will need to be -1 because example grid is not zero-indexed):
-#4.	Grab number from current index (prev_number)
-#5.	Set position to next index
-#6.	Calculate number for index (prev_number * 252533, modulo 33554393)
-
-#Index movements in zero-indexed array:
-#0,0
-#1,0
-#0,1
-#2,0
-#1,1
-#0,2
-#3,0
-#2,1
-#1,2
-#0,3
-
-#So need to set x to max_number and count down from there.
-#Meanwhile y counts up t
+# For the future, look into modular exponentiation using .pow
